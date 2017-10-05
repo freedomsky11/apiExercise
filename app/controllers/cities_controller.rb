4e26bc63ee790df0abc185cf94b92e58e09f4264
freedomsky <hanwei_1989@126.com>
2017-10-05 14:06:29 +0800

    implement update city's temperature data

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,4 +4,16 @@ class CitiesController < ApplicationController
     @cities = City.all
   end
 
+  def update_temp
+    @city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index", :params => { :cityname => @city.juhe_id, :key => "058947fb3682cd33e97e9938dd5880f2"}
+
+    data = JSON.parse(response.body)
+
+    @city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+  end
+
 end
